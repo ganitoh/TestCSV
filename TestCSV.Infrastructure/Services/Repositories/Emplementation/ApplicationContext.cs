9b79897bf22fb2260b88e035bb0fb0ff0dc9e6db
Vadim <vadim.gan2018@yandex.ru>
2024-04-22 17:01:55 +0300

    fix bags

--- TestCSV.Infrastructure/Services/Repositories/Emplementation/ApplicationContext.cs
+++ TestCSV.Infrastructure/Services/Repositories/Emplementation/ApplicationContext.cs
@@ -11,7 +11,6 @@ namespace TestCSV.Infrastructure.Services.Repositories.Emplementation
         public ApplicationContext(DbContextOptions options)
             : base(options)
         {
-            Database.EnsureDeleted();
             Database.EnsureCreated();
         }
 
